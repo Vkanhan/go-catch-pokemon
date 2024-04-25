302eb5aa1f07687326cdebb3e69135bed081601d
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-04-25 10:29:34 +0530

    feat: Enhance startRepl function to handle command execution based on user input

--- repl.go
+++ repl.go
@@ -17,10 +17,42 @@ func startRepl() {
 		text := scanner.Text()
 
 		cleaned := cleanInput(text)
+		if len(cleaned) == 0 {
+			continue
+		}
 
-		fmt.Println("echoing: ", cleaned)
+		commandName := cleaned[0]
+
+		availableCommands := getCommands()
+
+		command, ok := availableCommands[commandName] 
+		if !ok {
+			fmt.Println("invalid command")
+			continue
+		}
+		command.callback()
 	}
+}
 
+type cliCommand struct {
+	name string
+	description string
+	callback func()
+}
+
+func getCommands()map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name : "help",
+			description: "Prints the help menu",
+			callback: callbackHelp,
+		},
+		"exit": {
+			name : "exit",
+			description : "Turns off the pokedox",
+			callback: callBackExit,
+		},
+	}
 }
 
 func cleanInput(text string) []string {
