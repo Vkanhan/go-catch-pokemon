a23101038314a7c8d3fb297aa8e6498ac9c3cc7d
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-04-25 10:23:46 +0530

    feat: Add callbackHelp function to display help menu with available commands

---
+++ commandHelp.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func callbackHelp() {
+	fmt.Println("Welcome to the pokedox help menu!")
+	fmt.Println("Here are the available commands")
+
+	availableCommands := getCommands()
+	for _, cmd := range availableCommands {
+		fmt.Printf("- %s: %s\n", cmd.name, cmd.description)
+	} 
+
+	fmt.Println("")
+}
