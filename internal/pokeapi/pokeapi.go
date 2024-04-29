06bbdb1c28bf7261c331c48a78363fa1dc39079f
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-04-30 03:53:12 +0530

    feat(pokeapi): introduce cache support in Client

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -3,16 +3,20 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/Vkanhan/go-pokedox/internal/pokecache"
 )
 
 const baseURL = "https://pokeapi.co/api/v2"
 
 type Client struct {
+	cache      pokecache.Cacheinternal/pokeapi/pokeapi.go
 	httpClient http.Client
 }
 
-func NewClient() Client {
+func NewClient(cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: time.Minute,
 		},
