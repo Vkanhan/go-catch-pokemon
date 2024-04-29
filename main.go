21c0de01ae933c861bed4a38060560d297313ec4
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-04-30 03:55:10 +0530

    feat: initialize pokeapi.Client with cache support

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/Vkanhan/go-pokedox/internal/pokeapi"
+import (
+	"time"
+
+	"github.com/Vkanhan/go-pokedox/internal/pokeapi"
+)
 
 type config struct {
 	pokeapiClient pokeapi.Client 
@@ -11,7 +15,7 @@ type config struct {
 func main() {
 
 	cfg := config{
-		pokeapiClient: pokeapi.NewClient(),
+		pokeapiClient: pokeapi.NewClient(time.Hour),
 	}
 
 	startRepl(&cfg)
