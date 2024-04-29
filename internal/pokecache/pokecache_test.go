1cf9b7b943583f3f5fb7d1644fcfa49f2fce51b7
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-04-30 03:56:39 +0530

    Add test coverage to pokecache

---
+++ internal/pokecache/pokecache_test.go
@@ -0,0 +1,70 @@
+package pokecache
+
+import (
+	"testing"
+	"time"
+)
+
+func TestCreateCache(t *testing.T) {
+	cache := NewCache(time.Millisecond)
+	if cache.cache == nil {
+		t.Error("cache is nil")
+	}
+}
+
+func TestAddGetCache(t *testing.T) {
+	cache := NewCache(time.Millisecond)
+
+	cases := []struct {
+		inputKey string
+		inputVal []byte
+	}{
+		{
+			inputKey: "key1",
+			inputVal: []byte("val1"),
+		},
+		{
+			inputKey: "key2",
+			inputVal: []byte("val2"),
+		},
+		{
+			inputKey: "",
+			inputVal: []byte("val3"),
+		},
+	}
+
+	for _, cas := range cases {
+		cache.Add(cas.inputKey, cas.inputVal)
+		actual, ok := cache.Get(cas.inputKey)
+		if !ok {
+			t.Errorf("%s not found", cas.inputKey)
+			continue
+		}
+		if string(actual) != string(cas.inputVal) {
+			t.Errorf("%s doesnt match %s", string(actual), string(cas.inputVal),)
+			continue
+		}
+	}
+}
+
+
+func TestReapLoop(t *testing.T) {
+	const baseTime = 5 * time.Millisecond
+	const waitTime = baseTime + 5*time.Millisecond
+	cache := NewCache(baseTime)
+	cache.Add("key1", []byte("val1"))
+
+	_, ok := cache.Get("key1")
+	if !ok {
+		t.Error("expected to find key")
+		return
+	}
+
+	time.Sleep(waitTime)
+
+	_, ok = cache.Get("key1")
+	if ok {
+		t.Error("expected to not find key")
+		return
+	}
+}
