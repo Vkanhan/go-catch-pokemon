6e790b4d9c2a65d5b8dadf66182a5c180f47a49f
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-05-01 10:58:13 +0530

    feat: Improve Pokémon catching mechanism

--- commandCatch.go
+++ commandCatch.go
@@ -10,6 +10,7 @@ func commandCatch(cfg *config, args ...string) error {
 	if len(args) != 1 {
 		return errors.New("no pokemon name provided")
 	}
+
 	pokemonName := args[0]
 
 	pokemon, err := cfg.pokeapiClient.GetPokemon(pokemonName)
@@ -17,14 +18,17 @@ func commandCatch(cfg *config, args ...string) error {
 		return err
 	}
 
-	const threshold = 50
 	randomNum := rand.Intn(pokemon.BaseExperience)
-	fmt.Println(threshold, randomNum, pokemon.BaseExperience)
-	if randomNum > threshold {
-		return fmt.Errorf("failed to catch %s\n", pokemonName)
+
+	fmt.Printf("Throwing a Pokeball at %s...\n", pokemon.Name)
+	if randomNum > 40 {
+		fmt.Printf("%s escaped!\n", pokemon.Name)
+		return nil
 	}
 
-	cfg.caughtPokemon[pokemonName] = pokemon
-	fmt.Printf("%s was caught!\n", pokemonName)
+	fmt.Printf("%s was caught!\n", pokemon.Name)
+	fmt.Println("You may now inspect it with the inspect command.")
+
+	cfg.caughtPokemon[pokemon.Name] = pokemon
 	return nil
 }
